8b993de356350e27442019a8efdffbd206b9f62a
Ramon Aguilar <rc.aguilar.ramon@leadps.org>
2016-03-07 04:03:28 +0000

    add this please

---
+++ 4-1ReadingFiles/chhoseHaiku.py
@@ -0,0 +1,12 @@
+# We ask the user for an input
+print("Hi welcome to your new personal haiku reader")
+print("Please choose either option #3 which is a silly haiku, or option #6 which is a haiku about writing a haiku")
+haiku = int(raw_input())
+# We open both text files here
+haiku3 = open("haiku3.txt", "r")
+haiku6 = open("haiku6.txt", "r") 
+# here we make an if statement
+if haiku == 3
+	print(myHaiku3.read())
+else:
+	print(myHaiku6.read())
